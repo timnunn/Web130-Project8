339bf2736a4f03c2e45e05367c9c967b3eea9cd7
Timothy Nunn <54078499+timnunn@users.noreply.github.com>
2021-11-27 18:53:33 -0500

    Add files via upload

---
+++ Project8TN/Project8TN.js
@@ -0,0 +1,228 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"Project8TN_atlas_", frames: [[0,0,94,36],[0,38,52,52]]}
+];
+
+
+// symbols:
+
+
+
+(lib.CachedTexturedBitmap_10 = function() {
+	this.initialize(ss["Project8TN_atlas_"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedTexturedBitmap_11 = function() {
+	this.initialize(ss["Project8TN_atlas_"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.RedBall = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.instance = new lib.CachedTexturedBitmap_11();
+	this.instance.parent = this;
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,26,26);
+
+
+(lib.Scene_1_Layer_1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.instance = new lib.CachedTexturedBitmap_10();
+	this.instance.parent = this;
+	this.instance.setTransform(111.6,85.3,0.5,0.5);
+
+	this.instance_1 = new lib.RedBall("synched",0,false);
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(24.45,154.55,1,1,0,0,0,13.1,13.1);
+	this.instance_1._off = true;
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.instance}]},1).to({state:[{t:this.instance_1}]},71).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_1}]},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(72).to({_off:false},0).wait(1).to({regX:13,regY:13,x:25.4,y:151.3},0).wait(1).to({x:26.55,y:148.15},0).wait(1).to({x:27.65,y:145},0).wait(1).to({x:28.8,y:141.9},0).wait(1).to({x:30,y:138.8},0).wait(1).to({x:31.15,y:135.7},0).wait(1).to({x:32.35,y:132.6},0).wait(1).to({x:33.55,y:129.5},0).wait(1).to({x:34.8,y:126.4},0).wait(1).to({x:36.05,y:123.3},0).wait(1).to({x:37.35,y:120.25},0).wait(1).to({x:38.65,y:117.2},0).wait(1).to({x:39.95,y:114.15},0).wait(1).to({x:41.3,y:111.1},0).wait(1).to({x:42.65,y:108.05},0).wait(1).to({x:44.05,y:105.05},0).wait(1).to({x:45.5,y:102.05},0).wait(1).to({x:46.95,y:99.05},0).wait(1).to({x:48.4,y:96.05},0).wait(1).to({x:49.9,y:93.1},0).wait(1).to({x:51.45,y:90.15},0).wait(1).to({x:53,y:87.2},0).wait(1).to({x:54.6,y:84.3},0).wait(1).to({x:56.2,y:81.4},0).wait(1).to({x:57.85,y:78.5},0).wait(1).to({x:59.55,y:75.65},0).wait(1).to({x:61.3,y:72.8},0).wait(1).to({x:63.05,y:70},0).wait(1).to({x:64.85,y:67.2},0).wait(1).to({x:66.75,y:64.45},0).wait(1).to({x:68.65,y:61.7},0).wait(1).to({x:70.6,y:59},0).wait(1).to({x:72.55,y:56.35},0).wait(1).to({x:74.6,y:53.75},0).wait(1).to({x:76.7,y:51.15},0).wait(1).to({x:78.85,y:48.6},0).wait(1).to({x:81.1,y:46.15},0).wait(1).to({x:83.35,y:43.7},0).wait(1).to({x:85.7,y:41.35},0).wait(1).to({x:88.1,y:39.05},0).wait(1).to({x:90.55,y:36.8},0).wait(1).to({x:93.1,y:34.65},0).wait(1).to({x:95.7,y:32.6},0).wait(1).to({x:98.35,y:30.6},0).wait(1).to({x:101.1,y:28.75},0).wait(1).to({x:103.95,y:27},0).wait(1).to({x:106.85,y:25.35},0).wait(1).to({x:109.8,y:23.85},0).wait(1).to({x:112.85,y:22.5},0).wait(1).to({x:115.95,y:21.3},0).wait(1).to({x:119.1,y:20.25},0).wait(1).to({x:122.3,y:19.4},0).wait(1).to({x:125.55,y:18.7},0).wait(1).to({x:128.85,y:18.15},0).wait(1).to({x:132.15,y:17.85},0).wait(1).to({x:135.45,y:17.65},0).wait(1).to({x:138.8,y:17.7},0).wait(1).to({x:142.1,y:17.9},0).wait(1).to({x:145.4,y:18.25},0).wait(1).to({x:148.7,y:18.8},0).wait(1).to({x:151.95,y:19.5},0).wait(1).to({x:155.15,y:20.35},0).wait(1).to({x:158.35,y:21.3},0).wait(1).to({x:161.5,y:22.4},0).wait(1).to({x:164.6,y:23.65},0).wait(1).to({x:167.6,y:25},0).wait(1).to({x:170.6,y:26.4},0).wait(1).to({x:173.55,y:27.95},0).wait(1).to({x:176.5,y:29.55},0).wait(1).to({x:179.35,y:31.25},0).wait(1).to({x:182.15,y:33.05},0).wait(1).to({x:184.9,y:34.85},0).wait(1).to({x:187.65,y:36.8},0).wait(1).to({x:190.35,y:38.75},0).wait(1).to({x:192.95,y:40.75},0).wait(1).to({x:195.55,y:42.85},0).wait(1).to({x:198.15,y:44.95},0).wait(1).to({x:200.65,y:47.1},0).wait(1).to({x:203.15,y:49.3},0).wait(1).to({x:205.6,y:51.55},0).wait(1).to({x:208.05,y:53.85},0).wait(1).to({x:210.45,y:56.15},0).wait(1).to({x:212.8,y:58.5},0).wait(1).to({x:215.1,y:60.85},0).wait(1).to({x:217.45,y:63.25},0).wait(1).to({x:219.7,y:65.65},0).wait(1).to({x:221.95,y:68.1},0).wait(1).to({x:224.15,y:70.6},0).wait(1).to({x:226.35,y:73.1},0).wait(1).to({x:228.55,y:75.6},0).wait(1).to({x:230.7,y:78.15},0).wait(1).to({x:232.85,y:80.7},0).wait(1).to({x:234.95,y:83.25},0).wait(1).to({x:237.05,y:85.85},0).wait(1).to({x:239.1,y:88.45},0).wait(1).to({x:241.15,y:91.1},0).wait(1).to({x:243.15,y:93.7},0).wait(1).to({x:245.15,y:96.35},0).wait(1).to({x:247.15,y:99},0).wait(1).to({x:249.15,y:101.7},0).wait(1).to({x:251.1,y:104.4},0).wait(1).to({x:253.05,y:107.1},0).wait(1).to({x:254.95,y:109.8},0).wait(1).to({x:256.9,y:112.5},0).wait(1).to({x:258.75,y:115.25},0).wait(1).to({x:260.65,y:118},0).wait(1).to({x:262.5,y:120.75},0).wait(1).to({x:264.35,y:123.5},0).wait(1).to({x:266.2,y:126.25},0).wait(1).to({x:268.05,y:129.05},0).wait(1).to({x:269.85,y:131.85},0).wait(1).to({x:271.65,y:134.65},0).wait(1).to({x:273.45,y:137.45},0).wait(1).to({x:275.2,y:140.25},0).wait(1).to({x:276.95,y:143.05},0).wait(1).to({x:278.75,y:145.9},0).wait(1).to({x:280.45,y:148.75},0).wait(1).to({x:282.2,y:151.55},0).wait(1).to({x:283.95,y:154.45},0).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+
+
+// stage content:
+(lib.Project8TN = function(mode,startPosition,loop) {
+if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
+
+	this.___GetDepth___ = function(obj) {
+		var depth = obj.depth;
+		var cameraObj = this.___camera___instance;
+		if(cameraObj && cameraObj.depth && obj.isAttachedToCamera)
+		{
+			depth += depth + cameraObj.depth;
+		}
+		return depth;
+		}
+	this.___needSorting___ = function() {
+		for (var i = 0; i < this.getNumChildren() - 1; i++)
+		{
+			var prevDepth = this.___GetDepth___(this.getChildAt(i));
+			var nextDepth = this.___GetDepth___(this.getChildAt(i + 1));
+			if (prevDepth < nextDepth)
+				return true;
+		}
+		return false;
+	}
+	this.___sortFunction___ = function(obj1, obj2) {
+		return (this.exportRoot.___GetDepth___(obj2) - this.exportRoot.___GetDepth___(obj1));
+	}
+	this.on('tick', function (event){
+		var curTimeline = event.currentTarget;
+		if (curTimeline.___needSorting___()){
+			this.sortChildren(curTimeline.___sortFunction___);
+		}
+	});
+
+	// timeline functions:
+	this.frame_191 = function() {
+		this.___loopingOver___ = true;
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(191).call(this.frame_191).wait(1));
+
+	// Layer_1_obj_
+	this.Layer_1 = new lib.Scene_1_Layer_1();
+	this.Layer_1.name = "Layer_1";
+	this.Layer_1.parent = this;
+	this.Layer_1.depth = 0;
+	this.Layer_1.isAttachedToCamera = 0
+	this.Layer_1.isAttachedToMask = 0
+	this.Layer_1.layerDepth = 0
+	this.Layer_1.layerIndex = 0
+	this.Layer_1.maskLayerName = 0
+
+	this.timeline.addTween(cjs.Tween.get(this.Layer_1).wait(73).to({regX:154.2,regY:86.1,x:154.2,y:86.1},0).wait(119));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,297,167.5);
+// library properties:
+lib.properties = {
+	id: '2D3A7B16C90F8B4286CE12761423E956',
+	width: 300,
+	height: 200,
+	fps: 24,
+	color: "#0033FF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/Project8TN_atlas_.png", id:"Project8TN_atlas_"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['2D3A7B16C90F8B4286CE12761423E956'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+// Layer depth API : 
+
+AdobeAn.Layer = new function() {
+	this.getLayerZDepth = function(timeline, layerName)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline." + layerName + ".depth; else 0;";
+		return eval(script);
+	}
+	this.setLayerZDepth = function(timeline, layerName, zDepth)
+	{
+		const MAX_zDepth = 10000;
+		const MIN_zDepth = -5000;
+		if(zDepth > MAX_zDepth)
+			zDepth = MAX_zDepth;
+		else if(zDepth < MIN_zDepth)
+			zDepth = MIN_zDepth;
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline." + layerName + ".depth = " + zDepth + ";";
+		eval(script);
+	}
+	this.removeLayer = function(timeline, layerName)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline.removeChild(timeline." + layerName + ");";
+		eval(script);
+	}
+	this.addNewLayer = function(timeline, layerName, zDepth)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		zDepth = typeof zDepth !== 'undefined' ? zDepth : 0;
+		var layer = new createjs.MovieClip();
+		layer.name = layerName;
+		layer.depth = zDepth;
+		layer.layerIndex = 0;
+		timeline.addChild(layer);
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
